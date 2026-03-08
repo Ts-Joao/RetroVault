30054d39ce350922a2fb86dcc650bd1d415395c6
Hydrazl <joteixeirasimoes@gmail.com>
2026-03-07 22:02:34 -0300

    chore(seed): add media types seeds and improve admin seed

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -4,15 +4,29 @@ import { PrismaService } from '../src/database/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
-  const prisma = app.get(PrismaService);
+  const prisma = app.get<PrismaService>(PrismaService);
 
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: {
+      email: 'admin@retrovault.com',
+    },
+    update: {},
+    create: {
       name: 'Admin',
       email: 'admin@retrovault.com',
       password: 'adminpassword',
       role: 'ADMIN',
     },
+  });
+
+  await prisma.mediaType.createMany({
+    data: [
+      { name: 'GAME' },
+      {
+        name: 'MOVIE',
+      },
+    ],
+    skipDuplicates: true,
   });
 
   await app.close();
