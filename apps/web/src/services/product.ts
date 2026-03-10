bd2dd17913c5e281ad43fcce84cb19b62a82f7ea
Ts-Joao <cttjoaovts@gmail.com>
2026-03-10 17:43:11 -0300

    feat: adiciona Product interface e início do ProductCard

---
+++ apps/web/src/services/product.ts
@@ -0,0 +1,7 @@
+export async function getProducts() {
+    return mockProducts
+}
+
+const mockProducts = [
+    { title: 'Super Mario World', price: 120, image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR3RD8cpvrf8qjZMwSFD6QVsMmJir7aAUPM9SOyxEnB1mOCdINhFajHJZBcwG-zo9Wm5sOjICN2lNLO1h2PKueDKsh5Ra6pbdqHIbK2wmvstaTGCvHEFYe2_A' }
+]
