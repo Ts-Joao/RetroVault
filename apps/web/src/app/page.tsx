165a6516b27f1e62defaead13908746de1fd0ff6
Luiz henrique <luizhenriquesantana818@gmail.com>
2026-03-12 15:55:54 -0300

    feat: body-profile

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "@/components/layout/nav-bar/NavBar";
+import Profile from "./pages/profile/Profile";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Profile/>
     </>
   );
 }
