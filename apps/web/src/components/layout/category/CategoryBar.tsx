c9a30987bafc285d8a81e9fe57072147988567e7
Ts-Joao <cttjoaovts@gmail.com>
2026-03-13 15:42:54 -0300

    feat: resposividade completa no CategoryBar

--- apps/web/src/components/layout/category/CategoryBar.tsx
+++ apps/web/src/components/layout/category/CategoryBar.tsx
@@ -9,40 +9,40 @@ import {
 
 export default function CategoryBar() {
     return (
-        <div className="bg-[#d9d9d9] border-2 border-black text-second w-[60%] rounded-lg justify-self-center p-2 text-xs text-center font-chakra-petch">
+        <div className="bg-[#d9d9d9] border-2 border-black text-second md:w-[60%] lg:w-[50%] xl:w-[35%] rounded-lg justify-self-center p-2 text-xs lg:text-[13px] text-center font-chakra-petch">
             <div className="bg-[#d9d9d9] absolute mt-[-30] ml-2 p-1 h-6 border-2 rounded-b-none rounded-md border-black border-y-[#d9d9d9] border-t-black">
-                <p className="text-black">Categorias</p>
+                <p className="text-black text-[13px] lg:text-[15px]">Categorias</p>
             </div>
-            <ul className="flex justify-between px-3 z-2">
+            <ul className="flex justify-between px-3 z-2 gap-2 md:gap-0">
                 <li>
                     <Link href='/' className="flex justify-center">
-                        <PiGameController className="bg-third rounded-full w-12 h-12 p-2 text-3xl"/>
+                        <PiGameController className="bg-third rounded-full w-12 h-12 lg:h-15 lg:w-15 p-2 text-3xl"/>
                     </Link>
-                    <p className="text-black">Jogos</p>
+                    <p className="text-black hidden md:flex justify-center-safe">Jogos</p>
                 </li>
                 <li>
                     <Link href='/' className="flex justify-center">
-                        <PiCassetteTape className="bg-third rounded-full w-12 h-12 p-2 text-3xl"/>
+                        <PiCassetteTape className="bg-third rounded-full w-12 h-12 lg:h-15 lg:w-15 p-2 text-3xl"/>
                     </Link>
-                    <p className="text-black">Filmes</p>
+                    <p className="text-black hidden md:flex justify-center-safe">Filmes</p>
                 </li>
                 <li>
                     <Link href='/' className="flex justify-center">
-                        <PiChartBar className="bg-third rounded-full w-12 h-12 p-2 text-3xl"/>
+                        <PiChartBar className="bg-third rounded-full w-12 h-12 lg:h-15 lg:w-15 p-2 text-3xl"/>
                     </Link>
-                    <p className="text-black">Em Alta</p>
+                    <p className="text-black hidden md:flex justify-center-safe">Em Alta</p>
                 </li>
                 <li>
                     <Link href='/' className="flex justify-center">
-                        <PiSealPercent className="bg-third rounded-full w-12 h-12 p-2 text-3xl"/>
+                        <PiSealPercent className="bg-third rounded-full w-12 h-12 lg:h-15 lg:w-15 p-2 text-3xl"/>
                     </Link>
-                    <p className="text-black">Ofertas</p>
+                    <p className="text-black hidden md:flex justify-center-safe">Ofertas</p>
                 </li>
                 <li>
                     <Link href='/' className="flex justify-center">
-                        <PiTruck className="bg-third rounded-full w-12 h-12 p-2 text-3xl"/>
+                        <PiTruck className="bg-third rounded-full w-12 h-12 lg:h-15 lg:w-15 p-2 text-3xl"/>
                     </Link>
-                    <p className="text-black">Frete Grátis</p>
+                    <p className="text-black hidden md:flex">Frete Grátis</p>
                 </li>
             </ul>
         </div>
