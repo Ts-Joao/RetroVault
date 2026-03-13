9b2f771f75c07ea87e800ce643c61c1c19f83a9a
Luiz henrique <luizhenriquesantana818@gmail.com>
2026-03-13 15:43:34 -0300

    Fix:footer-responsivity

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/layout/nav-bar/NavBar";
 import Profile from "./pages/profile/Profile";
-import Footer from "@/components/Footer/Footer";
+import Footer from "@/components/layout/footer/Footer";
 
 export default function Home() {
   return (
