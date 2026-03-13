50232f8b88447b5c6760ac4aea38de12e5ecaa52
ktzxs <lucasalvesdacosta070@gmail.com>
2026-03-13 16:33:48 -0300

    inicio da page de Login🙌

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import Logo from "@/components/brand/Logo";
+import { FaUserAlt } from "react-icons/fa";
+
+export default function LoginPage() {
+    return (
+        <>
+
+            <main>
+                <Logo />
+
+                <FaUserAlt className="text-second" />
+            </main>
+        </>
+    );
+}
