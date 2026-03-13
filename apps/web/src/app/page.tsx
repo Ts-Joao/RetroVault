300b0559914a419777b6d11505baaf59a9d46b5a
Luiz henrique <luizhenriquesantana818@gmail.com>
2026-03-13 16:23:18 -0300

    fix:dir/fix

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,11 @@
 import NavBar from "@/components/layout/nav-bar/NavBar";
-import Profile from "./pages/profile/Profile";
 import Footer from "@/components/layout/footer/Footer";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Profile/>
+
       <Footer/>
     </>
   );
