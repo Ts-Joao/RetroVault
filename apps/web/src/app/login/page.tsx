e1cbd4c028551395028c9be6cb1365c8c0aefbfa
ktzxs <lucasalvesdacosta070@gmail.com>
2026-03-16 16:59:10 -0300

    tela de login ta quaseb feita🙌

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,15 +1,31 @@
-import Logo from "@/components/brand/Logo";
-import { FaUserAlt } from "react-icons/fa";
+import Image from "next/image";
+import { FaGoogle, FaInstagram, FaFacebook, FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function LoginPage() {
-    return (
-        <>
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100 gap-6">
+      <div className="bg-second p-10 rounded-lg w-[700px] h-[700px] text-white shadow-lg ">
+        
+        <Image className="ml-50 mt-[-10]"   
+          src="/logo.png"
+          alt="logo"
+          width={200}
+          height={200}
+        />
 
-            <main>
-                <Logo />
+        <div className="mb-4">
+            <label className="flex items-center gap-2 text-sm mb-1">
+                <MdEmail className="text-black text-3xl mt-5"/>
+                E-mail
+            </label>
 
-                <FaUserAlt className="text-second" />
-            </main>
-        </>
-    );
+            <input
+                className=""
+            />
+        </div>
+
+      </div>
+    </main>
+  )
 }
