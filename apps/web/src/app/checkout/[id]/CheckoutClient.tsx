4eebccfb8417a2f31cb400224ac5c1859157a1f7
Hydrazl <joteixeirasimoes@gmail.com>
2026-03-16 19:28:42 -0300

    feat: conclução da page checkout

--- apps/web/src/app/checkout/[id]/CheckoutClient.tsx
+++ apps/web/src/app/checkout/[id]/CheckoutClient.tsx
@@ -32,23 +32,31 @@ export default function CheckoutClient({product}: {product : Product}) {
                     <div className='bg-white flex relative items-center justify-center rounded-lg lg:rounded-2xl w-50 h-50 md:w-120 md:h-120'>
                         <Image src={product.photo} alt={product.name} fill className='object-contain' />
                     </div>
-                    <div className='lg:px-7 py-2 lg:py-5'>
-                        <h1 className='font-semibold text-3xl'>{product.name}</h1>
-                        <p>Por {product.seller}</p>
-                        <span><StarRating rating={product.rating}/></span>
+                    <div className='flex flex-col lg:px-7 py-2 lg:py-5 gap-13'>
+                        <div className='flex flex-col gap-4'>
+                            <h1 className='font-semibold text-3xl'>{product.name}</h1>
+                            <p className='text-lg'>Vendido por {product.seller}</p>
+                            <span className='text-lg'><StarRating rating={product.rating}/></span>
+                        </div>
 
-                        <p className='flex items-center gap-5'><PiTruck className='text-prim'/> Frete: <span>Caraguatatuba - São Paulo</span></p>
-                        <p className='flex items-center gap-5'><PiMoney className='text-prim'/> Valor: 0,00</p>
+                        <div className='grid gap-3'>
+                            <p className='flex items-center gap-5 text-md'><PiTruck className='text-prim lg:text-3xl'/> Frete: <span>Caraguatatuba - São Paulo</span></p>
+                            <p className='flex items-center gap-5 text-md'><PiMoney className='text-prim lg:text-3xl'/> Valor: <span>0,00</span></p>
+                        </div>
 
-                        <p className='flex items-center gap-2'>Quantidade: 
-                            <button onClick={decrement} className='cursor-pointer'><PiMinusSquare/></button>{quantity}
+                        <div className='flex items-center gap-4 text-lg'>
+                            <p>Quantidade:</p>
+                            <button onClick={decrement} className='cursor-pointer'><PiMinusSquare/></button>
+                            <span className='w-4 text-center inline-block'>{quantity}</span>
                             <button onClick={increment} className='cursor-pointer'><PiPlusSquare/></button>
-                        </p>
+                        </div>
 
-                        <div className='bg-gray-700 h-1px w-full'></div> {/* Separetor */}
-
-                        <h1 className='flex text-2xl md:font-semibold'>
-                            <span className='text-prim  md:mr-3'>R$</span>{reais}, <p className='text-third md:mr-3'>{centavos}</p> à vista</h1>
+                        <div className='grid gap-5 text-md'>
+                            <div className='bg-gray-700 h-0.5 w-full'></div> {/* Separetor */}
+                            <h1 className='flex text-3xl md:font-semibold'>
+                                <span className='text-prim md:mr-3'>R$</span>{reais}, <p className='text-third md:mr-3'>{centavos}</p> à vista
+                            </h1>
+                        </div>
                     </div>
                 </div>
 
