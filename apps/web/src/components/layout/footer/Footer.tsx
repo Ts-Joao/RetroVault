7b235880220a36d7401c41cc251c85fdac940d78
luiz <luiz.santana1@aluno.ifsp.edu.br>
2026-03-19 17:21:28 -0300

    refactor: diminuição do footer

--- apps/web/src/components/layout/footer/Footer.tsx
+++ apps/web/src/components/layout/footer/Footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export default function Footer() {
  return (
-  <footer className="bg-prim text-white py-6 px-4 lg:px-12 mt-50">
+  <footer className="bg-prim text-white py-5 px-4 lg:px-12">
    <div className="grid grid-cols-1 md:grid-cols-3 lg:flex-row items-center justify-between gap-10 md:gap-6">
     <div className="flex justify-center-safe">
         <Logo />
     </div>
-    <div className="grid md:flex justify-center text-center gap-1 md:gap-6 text-lg text-nowrap">
+    <div className="grid md:flex justify-center text-center gap-1 md:gap-6 text-md text-nowrap">
      <Link href="/">Termos de Uso</Link>
      <Link href="/">Política de Privacidade</Link>
      <Link href="/">Contato</Link>
     </div>
    </div>
-   <div className="text-center text-sm mt-6">
+   <div className="text-center text-xs mt-5">
     © Copyright - 2026 RetroVault & seus Parceiros
    </div>
   </footer>
