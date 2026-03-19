03db95ecd363d5e8135a3218d88c54ccba6694b9
Hydrazl <joteixeirasimoes@gmail.com>
2026-03-19 12:40:11 -0300

    refactor: useSelect mais flexível

--- packages/shared/src/hooks/useSelect.ts
+++ packages/shared/src/hooks/useSelect.ts
@@ -2,15 +2,18 @@
 
 import { useState } from 'react';
 
-export function useSelect(inital = '') {
+export function useSelect<T>(initial: T | null = null) {
     const [ isOpen, setIsOpen ] = useState(false)
-    const [ selected, setSelected ] = useState(inital)
+    const [selected, setSelected] = useState<T | null>(initial)
 
-    const toggle = () => setIsOpen(!isOpen)
-    const selectOption = (option: string) => {
+    const toggle = () => setIsOpen(prev => !prev)
+
+    const selectOption = (option: T) => {
         setSelected(option)
         setIsOpen(false);
     }
 
-    return { isOpen, selected, toggle, selectOption, }
+     const clear = () => setSelected(null)
+
+    return { isOpen, selected, toggle, selectOption, clear }
 }
