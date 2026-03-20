43da79d8c4cf97da42059a517a900fdf4728a9c7
luiz <luiz.santana1@aluno.ifsp.edu.br>
2026-03-20 17:28:30 -0300

    feat: completar carrinhp

--- apps/web/src/app/shopping/Shopping.tsx
+++ apps/web/src/app/shopping/Shopping.tsx
@@ -5,11 +5,14 @@ import ProductCart from "@/components/product-cart/ProductCart";
 export default function Shopping() {
   return (
     <>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center gap-3">
         <div className="flex flex-col items-center justify-center mt-10 gap-2">
           <ProductCart />
           <ProductCart />
         </div>
+        <div>
+        </div>
+
       </div>
     </>
   );
