cd0b7d084e31bb9986a2afbd7a15b33c5ccc48dd
Hydrazl <joteixeirasimoes@gmail.com>
2026-03-20 22:18:49 -0300

    refactor: refatoração da página e rota do carrinho

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,15 +3,13 @@ import NavBar from "@/components/layout/nav-bar/NavBar";
 import ProductGrid from "@/components/layout/product-grid/ProductGrid";
 import { getProducts } from "@/services/product";
 
-import Shopping from "./shopping/Shopping";
-
 export default async function Home() {
   const products = await getProducts()
 
   return (
     <>
       <NavBar />
-      <Shopping />
+      
     </>
   );
 }
