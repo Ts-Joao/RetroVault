55e7bd58adab55fc86f89e96472919c52185b874
Hydrazl <joteixeirasimoes@gmail.com>
2026-04-07 11:18:06 -0300

    feat(mobile): adiciona estilização do carrinho

--- apps/mobile/src/components/Header.tsx
+++ apps/mobile/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { View, TextInput, Image, StyleSheet } from "react-native";
+import { View, TextInput, Image, Text, Platform } from "react-native";
 import { MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
@@ -9,36 +9,25 @@ export default function Header() {
     const [cartOpen, setCartOpen] = useState(false);
     const insets = useSafeAreaInsets()
     const router = useRouter()
+    const padding = Platform.OS === 'web' ? 10 : insets.top
 
     return (
         <>
-            <View style={{ paddingTop: insets.top }} className="flex flex-row items-center text-[#374151] bg-primary py-3 justify-around">
+            <View style={{ paddingTop: padding }} className="flex flex-row items-center text-[#374151] bg-primary py-3 justify-around">
                 <Image source={require('../../assets/image/logo.png')} style={{width: 80, height: 40}} resizeMode="contain" />
                 <View className="flex flex-row bg-background-light px-2 rounded-lg items-center gap-2">
                     <TextInput placeholder="O que deseja hoje ?" className="py-2 px-3 min-w-48 max-w-48 placeholder:text-slate-500 text-slate-800 placeholder:font-chakra placeholder:text-md focus:outline-none" />
                     <Octicons name="search" size={18} onPress={() => router.navigate('/search')} />
                 </View>
-                <MaterialCommunityIcons name="cart-outline" size={24} color='#fff' onPress={() => setCartOpen(true)} />
+                <View className="relative">
+                    <MaterialCommunityIcons name="cart-outline" size={28} color='#fff' onPress={() => setCartOpen(true)} />
+                        <Text className="absolute text-center font-bold rounded-full border-2 border-white -right-2.5 -top-2.5 h-5 text-white w-5">
+                            2
+                        </Text>
+                </View>
             </View>
 
             <CartDrawer visible={cartOpen} onClose={() => setCartOpen(false)} />
         </>
     )
 }
-
-// const styles = StyleSheet.create({
-//     text_input: {
-//         flex: 1,
-//         fontFamily: 'Chackra',
-//         color: "#d9d9d9",
-//         fontSize: 20
-//     },
-//     container: {
-//         flex: 1,
-//         flexDirection: 'row',
-//         backgroundColor: '#F2EFDC',
-//         alignItems: 'center',
-//         maxHeight: 20,
-//         borderRadius: 4
-//     }
-// })
