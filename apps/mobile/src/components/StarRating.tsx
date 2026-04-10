3a84899a86e83aecbbd94299b6c772f774a88178
ts-joao <cttjoaovts@gmail.com>
2026-04-10 12:31:34 -0300

    fix(mobile): ajuste no tamanho da estrelas de avaliação

--- apps/mobile/src/components/StarRating.tsx
+++ apps/mobile/src/components/StarRating.tsx
@@ -3,19 +3,18 @@ import { Text } from "react-native";
 
 interface StarRatingProps {
     rating: number;
-    color?: string;
 }
 
-export default function StarRating({ rating, color = 'text-prim' }: StarRatingProps) {
+export default function StarRating({ rating }: StarRatingProps) {
     return (
         <Text className="flex">
             {[1, 2, 3, 4, 5].map((i) => {
                 if (rating >= i) {
-                    return <MaterialCommunityIcons name="star" key={i} color='#BF372A' />
+                    return <MaterialCommunityIcons name="star" key={i} size={16} color='#BF372A' />
                 } else if (rating >= i - 0.5) {
-                    return <MaterialCommunityIcons name="star-half-full" key={i} color="#BF372A" />
+                    return <MaterialCommunityIcons name="star-half-full" key={i} size={16} color="#BF372A" />
                 } else {
-                    return <MaterialCommunityIcons name="star-outline" key={i} color="#BF372A" />
+                    return <MaterialCommunityIcons name="star-outline" key={i} size={16} color="#BF372A" />
                 }
             })}
         </Text>
