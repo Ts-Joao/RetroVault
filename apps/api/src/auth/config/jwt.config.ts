a8f0f16c1ad3660d72e969f0a768235c610a1c82
ts-joao <cttjoaovts@gmail.com>
2026-06-08 16:46:10 -0300

    fix(api): corrige configuracao dos secrets do jwt

--- apps/api/src/auth/config/jwt.config.ts
+++ apps/api/src/auth/config/jwt.config.ts
@@ -1,30 +1,32 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('jwt', (): {
+export default registerAs(
+  'jwt',
+  (): {
     secret: string;
-    refreshSecret: string
+    refreshSecret: string;
     audience: string;
     issuer: string;
     ttl: string;
   } => {
     if (
-      !process.env.JWT_SECRET ||
+      !process.env.JWT_ACCESS_SECRET ||
       !process.env.JWT_REFRESH_SECRET ||
-      !process.env.JWT_AUDIENCE ||
-      !process.env.JWT_ISSUER ||
+      !process.env.JWT_TOKEN_AUDIENCE ||
+      !process.env.JWT_TOKEN_ISSUER ||
       !process.env.JWT_TTL
     ) {
       throw new Error(
-        'JWT_SECRET, JWT_AUDIENCE, JWT_ISSUER and JWT_TTL must be defined',
+        'JWT_ACCESS_SECRET, JWT_REFRESH_SECRET, JWT_TOKEN_AUDIENCE, JWT_TOKEN_ISSUER and JWT_TTL must be defined',
       );
     }
 
     return {
-      secret: process.env.JWT_SECRET,
+      secret: process.env.JWT_ACCESS_SECRET,
       refreshSecret: process.env.JWT_REFRESH_SECRET,
-      audience: process.env.JWT_AUDIENCE,
-      issuer: process.env.JWT_ISSUER,
+      audience: process.env.JWT_TOKEN_AUDIENCE,
+      issuer: process.env.JWT_TOKEN_ISSUER,
       ttl: process.env.JWT_TTL,
     };
-  }
+  },
 );
