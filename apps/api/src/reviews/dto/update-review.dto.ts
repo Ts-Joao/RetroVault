1073429c957846d9c9853714a4e8ce8d49955022
Ts-Joao <cttjoaovts@gmail.com>
2026-06-14 21:47:48 -0300

    fix(api): corrige status do pedido para pago quando pago usado a wallet

--- apps/api/src/reviews/dto/update-review.dto.ts
+++ apps/api/src/reviews/dto/update-review.dto.ts
@@ -1,8 +1,4 @@
-import { IsInt, Min, Max } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateReviewDto } from './create-review.dto';
 
-export class UpdateReviewDto {
-    @IsInt()
-    @Min(1)
-    @Max(5)
-    rating: number;
-}
+export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
