ffe44ce8b93f86c86aa70597e0861b8b88639250
ts-joao <cttjoaovts@gmail.com>
2026-06-16 16:00:34 -0300

    feat(api): implementa incio do sistema de copum

---
+++ apps/api/src/coupon/coupon.controller.ts
@@ -0,0 +1,75 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  Get,
+  Patch,
+  Delete
+} from '@nestjs/common';
+import { CouponService } from './coupon.service';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { PayloadDto } from 'src/auth/dto/payload.dto';
+import { AuthTokenGuard } from 'src/auth/guard/auth-token.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { AdminGuard } from 'src/common/guards/admin.guard';
+import { UpdateCouponDto } from './dto/update-coupon.dto';
+
+@UseGuards(AuthTokenGuard)
+@ApiBearerAuth()
+@Controller('coupon')
+export class CouponController {
+  constructor(private readonly couponService: CouponService) {}
+
+  @Post()
+  @UseGuards(AdminGuard)
+  create(@Body() createCouponDto: CreateCouponDto) {
+    return this.couponService.create(createCouponDto);
+  }
+
+  @Post('validate')
+  validateCoupon(
+    @Body('code') code: string,
+    @CurrentUser() user: PayloadDto,
+    @Body('orderTotal') orderTotal: number,
+  ) {
+    return this.couponService.validateCoupon(code, user.sub, orderTotal);
+  }
+
+  @Post('use/:code')
+  useCoupon(
+    @Param('code') code: string,
+    @CurrentUser() user: PayloadDto,
+    @Body('orderId') orderId: string,
+  ) {
+    return this.couponService.useCoupon(code, user.sub, orderId);
+  }
+
+  @Get()
+  findAll() {
+    return this.couponService.findAll();
+  }
+
+  @Get(':code')
+  findOne(@Param('code') code: string) {
+    return this.couponService.findOne(code);
+  }
+
+  @Patch(':code')
+  @UseGuards(AdminGuard)
+  update(
+    @Param('code') code: string,
+    @Body() updateCouponDto: UpdateCouponDto
+  ) {
+    return this.couponService.update(code, updateCouponDto);
+  }
+
+  @Delete(':code')
+  @UseGuards(AdminGuard)
+  remove(@Param('code') code: string) {
+    return this.couponService.remove(code);
+  }
+}
